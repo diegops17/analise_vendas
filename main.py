29e028a7ea3501a80215b4b4b9e06f44ba6c946d
Diêgo Souza <142118500+diegops17@users.noreply.github.com>
2026-02-18 20:39:21 +0000

    resolusão da análise

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ import openpyxl
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'junho']
 
 for mes in lista_meses:
-    print(mes)
+    #print(mes)
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    #print(tabela_vendas)
+
+    #O.any() significa que pega algum valor
+    condicao = (tabela_vendas['Vendas'] > 49980).any()
+
+    if condicao:
+        #print(f'No mês de {mes} alguém bateu a meta de 49980')
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 49980, 'Vendedor'].values[0] #O .loc é para pegar uma linha
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 49980, 'Vendas'].values[0]
+
+        print(f'No mês de {mes} o vendedor {vendedor}, total de vendas R$ {vendas} reais.')
